0d51b4e110d3e3dffa2889734a0a3d1ef7b11325
Vitalia Bilaniuk <vitaliiabilaniuk@gmail.com>
2018-03-27 12:34:19 +0300

    Affinity-team

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,55 +1,44 @@
-/*SLIDER NAVIGATION*/
-$(".carousel").on("touchstart", function(event){
-        var xClick = event.originalEvent.touches[0].pageX;
+jQuery(function ($) { 
+//Mobile carousel 
+  $(".carousel").on("touchstart", function(event){
+    var xClick = event.originalEvent.touches[0].pageX;
     $(this).one("touchmove", function(event){
-        var xMove = event.originalEvent.touches[0].pageX;
-        if( Math.floor(xClick - xMove) > 5 ){
-            $(this).carousel('next');
-        }
-        else if( Math.floor(xClick - xMove) < -5 ){
-            $(this).carousel('prev');
-        }
+    var xMove = event.originalEvent.touches[0].pageX;
+    if( Math.floor(xClick - xMove) > 5 ){
+        $(this).carousel('next');
+    }
+    else if( Math.floor(xClick - xMove) < -5 ){
+        $(this).carousel('prev');
+    }
     });
     $(".carousel").on("touchend", function(){
             $(this).off("touchmove");
     });
-});
-
-/*Change mobile menu-icon to cross icon*/
-jQuery(function ($) { 
-    $('#ChangeToggle').click(function() {
-        $('#navbar-hamburger').toggleClass('hidden');
-        $('#navbar-close').toggleClass('hidden');
-        $('.main-bg').toggleClass('menu-opened');
-    });
-/*Carousel enable */
-    $(document).ready(function(){
-        $('.center').slick({
-            centerMode: true,
-            centerPadding: '20px',
-            slidesToShow: 5,
-            responsive: [
-              {
-                breakpoint: 1200,
-                settings: {
-                  arrows: true,
-                  centerMode: true,
-                  centerPadding: '120px',
-                  slidesToShow: 3
-                }
-              },
-              {
-                breakpoint: 991,
-                settings: {
-                  arrows: true,
-                  centerMode: true,
-                  centerPadding: '80px',
-                  slidesToShow: 3
-                }
-              }
-            ]
+  });
+  //Desktop carousel
+    $(document).ready(function() {
+        $(document).ready(function () {
+            var carousel = $("#carousel").waterwheelCarousel({
+              flankingItems: 3,
+              dotsContainer: '#carousel-custom-dots',
+            });
+    
+            $('#prev').bind('click', function () {
+              carousel.prev();
+              return false
+            });
+    
+            $('#next').bind('click', function () {
+              carousel.next();
+              return false;
+            });
           });
-      });
+    });
+    //Popup
+    $( "header button" ).click(function() {
+      $( ".popup" ).toggle();
+    });
+    $( "#closePopup" ).click(function() {
+      $( ".popup" ).css("display", "none");
+    });
 });
-
-
